c5132777750e4a37a079e5c4a9ad00cf35ccf4ff
Vinod D Pai <44208193+vinod-dpai@users.noreply.github.com>
2022-11-11 23:15:31 +0530

    Updates

---
+++ precache-manifest.a53a2f3ed297470bf8c3a41d9b6d6587.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7241ba10a776856f1d01",
+    "url": "/learn/static/css/main.790d5f69.chunk.css"
+  },
+  {
+    "revision": "7241ba10a776856f1d01",
+    "url": "/learn/static/js/main.6d2fdfd1.chunk.js"
+  },
+  {
+    "revision": "dafb76f11fe4b0945190",
+    "url": "/learn/static/js/runtime~main.c1b4717c.js"
+  },
+  {
+    "revision": "a0631d969bc6d926dd95",
+    "url": "/learn/static/css/2.f8af5f14.chunk.css"
+  },
+  {
+    "revision": "a0631d969bc6d926dd95",
+    "url": "/learn/static/js/2.3eb65d2e.chunk.js"
+  },
+  {
+    "revision": "5656dea25dd962128fb3",
+    "url": "/learn/static/js/3.cb9903fc.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/learn/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "455e6776990b1303fb4201cec51af432",
+    "url": "/learn/static/media/kelsa.455e6776.png"
+  },
+  {
+    "revision": "32b9bc2104ad8f5011292d44816f7c54",
+    "url": "/learn/static/media/menu.32b9bc21.webp"
+  },
+  {
+    "revision": "2bf0084be967aaace2b0ffbb0f293dbf",
+    "url": "/learn/static/media/filter.2bf0084b.webp"
+  },
+  {
+    "revision": "55c8a29eda440ebeca2d6520df7fa215",
+    "url": "/learn/index.html"
+  }
+];
