3c06d238c3246daf096d91d11422b466bb6981f6
Vinod D Pai <44208193+vinod-dpai@users.noreply.github.com>
2022-12-26 22:33:21 +0530

    Added about section

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,9 +1,84 @@
 import React from 'react';
 
 const About = () => (
-  <div>
+  <div style={{ paddingInline: '2rem'}}>
     <h1>About</h1>
-    
+    <div>
+      <p style={{ textIndent: '2rem' }}>
+        Legal Awareness Portal (LEAP) is created by the District Legal Services Authority Alappuzha (DLSA Alappuzha) with an intention to leap towards legal awareness among the public  and students. The DLSA is a district level agency of National Legal Services Authority (NALSA).
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        The National Legal Services Authority (NALSA) has been constituted under the Legal Services Authorities Act, 1987 to provide free Legal Services to the weaker sections of the society and to organize Lok Adalats for amicable settlement of disputes.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        In every State, State Legal Services Authority has been constituted to give effect to the policies and directions of the NALSA and to give free legal services to the people and conduct Lok Adalats in the State.  The State Legal Services Authority is headed by Hon’ble the Chief Justice of the respective High Court who is the Patron-in-Chief of the State Legal Services Authority.A Senior Most Judge of High Court is nominated as Executive Chairman, SLSA.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        In every District, District Legal Services Authority has been constituted to implement Legal Services Programmes in the District. The District Legal Services Authority is situated in the District Courts Complex in every District and chaired by the District Judge of the respective district.A Judicial Officer  in the cadre of Sub Judge is appointed as Secretary on full time basis. In every Taluk, Taluk Legal Services committees are working under the chairmanship of the senior most Judicial Officer in the Taluk.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        The National Legal Services Authority (NALSA) is the Central Authority to provide free legal aid to poor, weaker sections and people in distress. The objective of NALSA is to establish a nation-wide network for providing free competent and comprehensive legal services.
+      </p>
+      <h3>Major Functions of NALSA</h3>
+      <ul>
+        <li>
+          To issue guidelines for the State Legal Services Authorities to implement the Legal Aid programmes and Schemes throughout the country.
+        </li>
+        <li>
+          Lay down policies and principles for making legal services available to the people.
+        </li>
+        <li>
+          Undertake and promote research in the field of legal services with special reference to the need for such services among the poor.
+        </li>
+        <li>
+          Take appropriate measures for spreading legal literacy and legal awareness amongst the people particularly the weaker sections.
+        </li>
+        <li>
+          Encourage settlement of disputes by way of negotiations, arbitration and conciliation.
+        </li>
+      </ul>
+      <p>For more details about NALSA please visit official website : <a href="https://www.nalsa.gov.in/" target="_blank">www.nalsa.gov.in</a></p>
+      <h3>Functions of Legal Services Authority at State Level</h3>
+      <p style={{ textIndent: '2rem' }}>
+        State Legal Services Authority, District Legal Services Authorities and Taluka/Sub Division Legal Services Committees have been assigned the task of discharging the two main functions on regular basis:
+      </p>
+      <ul>
+        <li>To provide free Legal Services to the eligible persons.</li>
+        <li>To organize Lok Adalats for amicable settlement of disputes.</li>
+      </ul>
+      <p>For more details about Kerala State Legal Services Authority(KELSA) please visit official website : <a href="https://www.kelsa.nic.in/" target="_blank">www.kelsa.nic.in</a></p>
+      <p>Eligible persons for free legal services include:</p>
+      <ul>
+        <li>Persons whose annual income does not exceed Rs.3,00,000/-</li>
+        <li>Women and Children irrespective of economic status.</li>
+        <li>Members of scheduled Castes and Scheduled Tribes.</li>
+        <li>Disabled persons.</li>
+        <li>Industrial workmen.</li>
+        <li>Victims of mass disaster, violence, floods, drought, earth quake, industrial violence.</li>
+        <li>Victims of Trafficking in Human Beings or Beggars.</li>
+      </ul>
+      <h3>How to make use of free Legal aid programme?</h3>
+      <p style={{ textIndent: '2rem' }}>
+        An eligible person of this programme can approach or write to the Legal Services Authority at the state or Taluk/Sub Division Level with details of Legal case or dispute or any other Legal matter covered under this programme and make request for justice. if the case is accepted, the case of the victim will be taken up in the concerned court for which all expenses including the fees of the advocate will be borne by the authority. Cases may also be referred to Lok Adalats for quicker justice. In either situation, the victim will get justice free of any charges.
+      </p>
+      <h3>DISTRICT LEGAL SERVICES AUTHORITY (DLSA), ALAPPUZHA</h3>
+      <p style={{ textIndent: '2rem' }}>
+        District Legal Services Authority is constituted under the Legal Services Authorities Act 1987(as amended by Amendment Act 1994 (No. 59/1994) to provide a free and competent legal aid to the weaker section of the society to ensure that opportunities for securing justice are not denied to any citizen by reason of economic or other disabilities and to organise Lok adalats to secure that the operation of the legal system promotes justice on the basis of equal opportunity.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        District Legal Services Authority, Alappuzha  is functioning under the chairmanship of Principal District Judge, Alappuzha.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        A Judicial member of the rank of Sub Judge designated as Secretary is leading all the functions of District Legal Services Authority, Alappuzha.
+      </p>
+      <p style={{ textIndent: '2rem' }}>
+        DLSA is conducting legal literacy classes in various laws and legal aid to the down trodden people. DLSA is active in conducting various legal literacy programmes as scheduled in the National Plan of Action by NALSA. DLSA have Para Legal Volunteers who are working in various fields. DLSA and TLSCs (Taluk Legal Services Committees) have a separate panel of advocates for Legal Literacy Classes, Legal Aid Counsel for civil and criminal cases and legal counsels to attend legal aid clinics. DLSA and TLSCs are conducting regular Lok Adalath on every  week . Also conducts special Adalaths to settle pre-litigation matters and to settle all types of compoundable cases in various Courts.
+      </p>
+      <h3>Taluk Legal Services Committees</h3>
+      <p style={{ textIndent: '2rem' }}>
+        These are 6 Taluk Legal Services Committees (TLSC) in Alappuzha District. All the TLSCs are constituted under Chairmanship of Senior Judicial Officers in the Taluk.
+      </p>
+    </div>
   </div>
 );
 
